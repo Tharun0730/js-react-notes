1bcfd916bb3e8f366d4c3627092895c748ce70ec
Tharun <133158803+Tharun0730@users.noreply.github.com>
2026-09-04 17:45:47 +0530

    Practiced flatten nested array question

--- js-codes/flattingArray.js
+++ js-codes/flattingArray.js
@@ -13,17 +13,18 @@ function flatArray(arr,result){
 
 
 console.log(flatArray(arr,[]))
-//  function flatArray(arr,result=[]){
-//  for(let i of arr){
-//      if(Array.isArray(i)){
-//          return flatArray(i,result)
-//      }else{
-//          result.push(i)
-//      }
-     
-//  }   
- 
-//  return result 
+// const arr = [1, 2, 3, [3, [4], 4, 3], 3];
+
+// function flatten(arr, result = []) {
+//   for (const data of arr) {
+//     if (Array.isArray(data)) {
+//       flatten(data, result);
+//     } else {
+//       result.push(data);
+//     }
+//   }
+
+//   return result;
 // }
 
-// console.log(flatArray(arr))
+// console.log(flatten(arr));
